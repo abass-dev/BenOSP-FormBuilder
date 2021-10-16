58762aad754eed14703b8a99db90ed977e74cfa7
Abass Ben Cheik <cheikabassben@gmail.com>
2021-10-16 18:04:43 +0000

    feat: add datetime input type

---
+++ src/Type/DateTimeType.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * This file is part of the BenOSP
+ *
+ * (c) Abass Ben Cheik <abass@todaysdev.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+declare(strict_types = 1);
+
+namespace BenOSP\Type;
+
+use BenOSP\Type\BaseType;
+
+/**
+ * {@inheritDoc}, input text type
+ *
+ * @author Abass Ben Cheik <abass@todaysdev.com>
+ */
+class DateTimeType extends BaseType
+{
+    
+    /** {@inheritDoc} */
+    public function renderInput(): string
+    {
+         return sprintf('<input id="%s" name="%s" type="date" class="form-control">', $this->id, $this->name);
+    }
+}
